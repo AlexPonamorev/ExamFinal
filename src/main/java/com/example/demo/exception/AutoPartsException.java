f4fc76ebe9e0a7bfbf35127124c4ceb004f0d4b3
AlexPonamorev <ponamorev.alex2012@gmail.com>
2021-06-07 23:42:11 +0300

    исправлены опечатки в названиях

--- src/main/java/com/example/demo/exception/AutoPartsException.java
+++ src/main/java/com/example/demo/exception/AutoPartsException.java
@@ -1,4 +1,4 @@
-package com.example.demo.exeption;
+package com.example.demo.exception;
 
 public class AutoPartsException extends RuntimeException{
     public AutoPartsException(String message){super(message);}
--- src/main/java/com/example/demo/service/ServiceQuery.java
+++ src/main/java/com/example/demo/service/ServiceQuery.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.AutoParts;
-import com.example.demo.exeption.AutoPartsException;
+import com.example.demo.exception.AutoPartsException;
 import com.example.demo.repository.AutoPartsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
